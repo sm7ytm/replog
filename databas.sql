e1f0a77a9b034e69acdc3e5c6cff5b9ab8a5befd
sm7ytm <sm7ytm@gmail.com>
2017-02-09 19:56:18 +0100

    Filen för att skapa databasen med alla fält och en del dummydata för replog-programmet
    Denna fil ska kunna importeras i mysql för att bygga hela databasen innan körning första gången replog körs

---
+++ databas.sql
@@ -0,0 +1,12 @@
+-- # Fil för skapande av databasen för replog-programmet
+
+-- # Fält att skapa:	
+--
+--	repkod
+--	felkod
+--	maskintyp
+--	felbeskrivning
+--	koll_komp
+--	bytt_komp
+--	resultat
+	
