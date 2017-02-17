99099f283f765389719cda4678ff96cb02f02965
Magnus Jönsson <sm7ytm@gmail.com>
2017-02-17 22:15:51 +0100

    Modifierat variabelnamnen för att mysql ska godkänna dem, funkade tidigare på gamla systemet men inte nu.

--- visning.py
+++ visning.py
@@ -2,63 +2,63 @@
 # -*- coding: utf-8 -*-
 #
 #  visning.py
-#  
+#
 #  Copyright 2017 Magnus Jönsson <magnus@debian>
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
-#  
+#
+#
 
 from conn import config
 
 
 try:
-	
-	import pymysql
 
-	cnx = pymysql.connect(**config)
-	cnx.autocommit(True)
-	cur = cnx.cursor()
+    import pymysql
+
+    cnx = pymysql.connect(**config)
+    cnx.autocommit(True)
+    cur = cnx.cursor()
 except pymysql.err.OperationalError:
-	sys.exit("Invalid input: Wrong username/password")
+    print("Det gick inte")
+exit(0)
 
 kolumn = raw_input("Ange vilken kolumn du vill söka i: ")
 urval = raw_input("Ange den uppgift du vill se: ")
 
 
-print(kolumn,urval)
+print(kolumn, urval)
 
-sqlquery = ("SELECT * FROM reparation WHERE %s='%s';" % (kolumn,urval))
+sqlquery = ("SELECT * FROM reparation WHERE %s='%s';" % (kolumn, urval))
 
 try:
-	cur.execute(sqlquery)
-	print("Frågan kördes utan fel")
+    cur.execute(sqlquery)
+    print("Frågan kördes utan fel")
 except:
-	print("Det sket sig!")
-	
-	
-for Maskin_ID, Felkod, Maskintyp, Felbeskrivning, Kollade_Komponenter, Bytade_Komponenter,Resultat in cur:
-	print(Maskin_ID, Felkod, Maskintyp, Felbeskrivning, Kollade_Komponenter, Bytade_Komponenter,Resultat)
-	
+    print("Det sket sig!")
+
+
+for Maskin_ID, Felkod, Maskintyp, Felbeskrivning, Kollade_Komponenter, Bytade_Komponenter, Resultat in cur:
+    print(Maskin_ID, Felkod, Maskintyp, Felbeskrivning,
+          Kollade_Komponenter, Bytade_Komponenter, Resultat)
 
 
 def main():
-	
-	return 0
+
+    return 0
 
 if __name__ == '__main__':
-	main()
-
+    main()
